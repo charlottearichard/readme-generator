32f0b6b9d1b830ed1bed9f242bd0ada87e94b401
Charlotte <charlottealiciarichard@gmail.com>
2022-06-13 21:21:10 -0400

    questions & generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,40 +1,43 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input (Inquire Package)
 const questions = [];
 
-var inquirer = require("inquirer");
-inquirer.prompt([
-  // MAIN TITLE OF PROJECT
-  {
-    type: "input",
-    message: "What is the name of your project?",
-    name: "title",
-  },
+inquirer
+  .prompt([
+    // MAIN TITLE OF PROJECT
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "Project Title",
+    },
 
-  // LANGUAGES USED FOR PROJECT
-  {
-    type: "checkbox",
-    message: "What languages are you using?",
-    name: "languages",
-    choices: ["HTML", "JavaScript", "CSS"],
-  },
+    // LANGUAGES USED FOR PROJECT
+    {
+      type: "checkbox",
+      message: "What languages are you using?",
+      name: "Programing Languages",
+      choices: ["HTML", "JavaScript", "CSS"],
+    },
 
-  // PROJECT MOTIVATION
-  {
-    type: "input",
-    message: "What was the motivation behind this project?",
-    name: "motivation",
-  },
+    // PROJECT MOTIVATION
+    {
+      type: "input",
+      message: "What was the motivation behind this project?",
+      name: "Project Motivation",
+    },
 
-  //
+    //
 
-  {
-    type: "input",
-    message: "What problem does this project solve?",
-    name: "solution",
-  },
-]);
+    {
+      type: "input",
+      message: "What problem does this project solve?",
+      name: "Problems this project solves",
+    },
+  ])
+
+  .then((answers) => console.log(answers));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
