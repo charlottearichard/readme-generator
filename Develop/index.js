76c8c5478052b3d7454bb1c7596aa8d6671614a0
Charlotte <charlottealiciarichard@gmail.com>
2022-06-15 10:49:16 -0400

    Write ReadMe  File

--- Develop/index.js
+++ Develop/index.js
@@ -1,49 +1,63 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input (Inquire Package)
-const questions = [];
+const questions = [
+  // MAIN TITLE OF PROJECT
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "Title",
+  },
 
-inquirer
-  .prompt([
-    // MAIN TITLE OF PROJECT
-    {
-      type: "input",
-      message: "What is the name of your project?",
-      name: "Project Title",
-    },
+  // LANGUAGES USED FOR PROJECT
+  {
+    type: "checkbox",
+    message: "What languages are you using?",
+    name: "Languages",
+    choices: ["HTML", "JavaScript", "CSS"],
+  },
 
-    // LANGUAGES USED FOR PROJECT
-    {
-      type: "checkbox",
-      message: "What languages are you using?",
-      name: "Programing Languages",
-      choices: ["HTML", "JavaScript", "CSS"],
-    },
+  // PROJECT Description
+  {
+    type: "input",
+    message: "Please provide a description of your project:",
+    name: "Description",
+  },
 
-    // PROJECT MOTIVATION
-    {
-      type: "input",
-      message: "What was the motivation behind this project?",
-      name: "Project Motivation",
-    },
+  //INSTALLATION
+  {
+    type: "input",
+    message: "How to install project?",
+    name: "Installation",
+  },
+];
 
-    //
-
-    {
-      type: "input",
-      message: "What problem does this project solve?",
-      name: "Problems this project solves",
-    },
-  ])
-
-  .then((answers) => console.log(answers));
+// .then((answers) => console.log(answers));
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Successfully wrote: " + fileName);
+  });
+}
+
+// initialization function
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    writeToFile("README.md", generateMarkdown(data));
+  });
+}
+
+// run the app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,16 @@
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  # Description 
+  ${data.Languages}
+  ${data.Description}
+ 
+
+
+`;
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -9,12 +22,5 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
 
 module.exports = generateMarkdown;
